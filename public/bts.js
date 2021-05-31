9c8ebcb00277bc537eacb2c9c99d4a229fd4983d
anu-1998 <75777897+anu-1998@users.noreply.github.com>
2021-05-31 10:47:34 +0530

    Initial commit

---
+++ public/bts.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1 style={{ color: "red" }}>Bangtan Boys </h1>
+    <p>
+      {" "}
+      BTS, also known as the Bangtan Boys, is a seven-member South Korean boy
+      band that began formation in 2010 and debuted in 2013 under Big Hit
+      Entertainment.[4] The septet—composed of Jin, Suga, J-Hope, RM, Jimin, V,
+      and Jungkook—co-writes and co-produces much of their own output.
+      Originally a hip hop group, their musical style has evolved to include a
+      wide range of genres. Their lyrics, often focused on personal and social
+      commentary, touch on the themes of mental health, troubles of school-age
+      youth and coming-of-age, loss, the journey towards loving oneself, and
+      individualism. Their work features references to literature and
+      psychological concepts and includes an alternative universe storyline. The
+      group has released several albums and performed on several world tours.
+    </p>
+    <h1 style={{ color: "red" }}>Members</h1>
+    <ul>
+      <li>Tae hung</li>
+      <li>Jimin</li>
+      <li>Jhope</li>
+      <li>RM</li>
+      <li>kookie</li>
+      <li>suga</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
